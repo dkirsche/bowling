b83c53cc9ce1ffe62b56e43169d71aa1f352c922
dkirsche <dan@blinkbeverage.com>
2013-05-23 08:22:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = 'b2f8f872f904501e643ae5d75aac34034d122b0335d616b3048a130bf9a4b87ab81b979b1acc93c8da735b4f37b2101a317f7b71b3a15117b03f993d4c6d2094'
